d9cff18d17d58494172dec806d602c955b8d899d
Jovana-stack <jovanas384@gmail.com>
2024-08-28 18:37:24 +1200

    added missing dependencies

--- mappedin-quickstart/vite.config.js
+++ mappedin-quickstart/vite.config.js
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 3000, // Adjust the limit to suppress warnings
-  },
+    rollupOptions: {
+        external: ['react', 'react-dom'], // Add other external dependencies if needed
+      },  
+},
 });
