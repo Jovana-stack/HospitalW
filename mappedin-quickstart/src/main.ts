98f789e4618e61148ec99e63c24abe38f3cd771d
Jovana-stack <jovanas384@gmail.com>
2024-09-18 14:10:40 +1200

    eme3rgency button update

--- mappedin-quickstart/src/main.ts
+++ mappedin-quickstart/src/main.ts
@@ -210,6 +210,8 @@ async function init() {
   // Check URL parameters on initialization
   const urlParams = new URLSearchParams(window.location.search);
   const startSpaceIdFromUrl = urlParams.get("startSpace");
+  
+  
   const endSpaceIdFromUrl = urlParams.get("endSpace");
 
   if (startSpaceIdFromUrl) {
@@ -386,11 +388,11 @@ async function init() {
 
   //add an emergency square button here:
   const emergencyButton = document.createElement("button");
-  emergencyButton.className = "reset-button mi-button";
-  emergencyButton.textContent = "Emergency Exit";
-  //emergencyButton.style.position = "absolute";
+  emergencyButton.className = "emergency-btn";
+  //emergencyButton.textContent = "Emergency Exit";
+  
   emergencyButton.style.bottom = "55px";
-  //emergencyButton.style.right = "10px";
+  
   emergencyButton.style.zIndex = "1000";
   emergencyButton.style.padding = "10px";
   emergencyButton.style.backgroundColor = "#FF0000"; //red bg color
@@ -399,6 +401,14 @@ async function init() {
   //emergencyButton.style.borderRadius = "5px";
   emergencyButton.style.cursor = "pointer";
   emergencyButton.setAttribute("data-emergency-btn", "true");
+
+  // 2. Create the icon element
+  const iconEmergency = document.createElement("i");
+  iconEmergency.className = "fa fa-sign-out"; // Font Awesome class for the book icon
+  iconEmergency.style.fontSize = "28px"; // Set the font size
+
+  // Append the icon to the button
+  emergencyButton.appendChild(iconEmergency);
 
   // Append the button to the map container
   mappedinDiv.appendChild(emergencyButton);
@@ -412,7 +422,7 @@ async function init() {
         mapView.Paths.remove(path);
         path = null;
       }
-      emergencyButton.textContent = "Emergency Exit";
+      //emergencyButton.textContent = "Emergency Exit";
       emergencyButton.style.backgroundColor = "#FF0000"; //red bg color
       emergencyExitOn = false;
     } else {
@@ -489,7 +499,7 @@ async function init() {
             farRadius: 0.5,
             color: "red",
           });
-          emergencyButton.textContent = "Emergency Off";
+        //emergencyButton.textContent = "Emergency Off";
           emergencyButton.style.backgroundColor = "#28a745";
           emergencyExitOn = true;
         }
